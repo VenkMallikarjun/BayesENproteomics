ec0dc36b1abecc5e3faf9234608492dea7e78975
Venk <36739430+VenkMallikarjun@users.noreply.github.com>
2018-06-05 13:11:41 +0100

    Add files via upload

---
+++ Matlab/bhfdr.m
@@ -0,0 +1,10 @@
+function fdr = bhfdr(p)
+% Compute Benjamini-Hochberg FDRs
+nanidx = isnan(p);
+
+m = numel(p(~nanidx));
+[p_ord, idx] = sort(p);
+fdr_ord =  p_ord(:) .* (m./[(1:m),NaN(1,sum(nanidx,1))])';
+fdr(idx) = fdr_ord;
+fdr = fdr';
+end
