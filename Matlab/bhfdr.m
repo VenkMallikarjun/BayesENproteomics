aac92382cce4a3acbbcf36787257f3a48e790445
Venk <36739430+VenkMallikarjun@users.noreply.github.com>
2018-07-17 00:53:25 +0100

    behaviour now similar to mafdr()

--- Matlab/bhfdr.m
+++ Matlab/bhfdr.m
@@ -5,6 +5,7 @@ nanidx = isnan(p);
 m = numel(p(~nanidx));
 [p_ord, idx] = sort(p);
 fdr_ord =  min(1, p_ord(:) .* (m./[(1:m),NaN(1,sum(nanidx,1))])');
+fdr_ord = cummin(fdr_ord,'reverse');
 fdr(idx) = fdr_ord;
 fdr = fdr';
 end
